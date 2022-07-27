64e9492934e3577d7c04cabc1cb39d203baa8a52
oleqich <novikov01399@gmail.com>
2022-07-27 03:05:13 +0300

    and again...

--- klassy/settings.py
+++ klassy/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
